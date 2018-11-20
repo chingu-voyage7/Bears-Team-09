44abf5389e72740ef87ad98bde37cc68e64a31e0
lkaratun <lev.karatun@gmail.com>
2018-11-20 17:33:23 -0500

    First steps

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const { Client } = require("pg");
+const client = new Client();
+
+(async function() {
+  await client.connect();
+
+  const res = await client.query("SELECT $1::text as message", [
+    "Hello world!"
+  ]);
+  console.log(res.rows[0].message); // Hello world!
+  await client.end();
+})();
