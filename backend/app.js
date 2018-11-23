9ee841af2d0370625ea2cc23a2a103822d944a23
trolleksii <trolleksii@gmail.com>
2018-11-23 17:19:09 -0500

    add scaffold to support auth module

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express'),
+      app = express(),
+      authRouter = require('./routes/auth'),
+      usersRouter = require('./routes/protected'),
+      logger = require('morgan'),
+      bodyParser = require('body-parser'),
+      passport = require('./middleware/passport'),
+      port = 8000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+app.use('/auth', authRouter);
+app.use('/protected', passport.authenticate('jwt', {session: false}), usersRouter);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
