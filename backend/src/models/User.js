ad5fb6153718df538957fe5c2161f1a92fec14ce
trolleksii <trolleksii@gmail.com>
2018-12-02 21:34:09 -0500

    fixed sql-injection prone code, added basic validation

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,18 +1,23 @@
-const Table = require("./Table"),
+const Table = require('./Table'),
       jwt = require('jsonwebtoken'),
       SECRET = process.env.JWT_SECRET || 'Default_JWT-Secret',
-      JWT_EXP_THRESHOLD = process.env.JWT_EXP_THRESHOLD || '1 hour';
+      JWT_EXP_THRESHOLD = process.env.JWT_EXP_THRESHOLD || '1 day';
 
 class User extends Table {
   constructor(data) {
-    super();
-    this.data = data;
-    this.tableName = "users";
-    this.pk = "email";
+    const pk = 'email',
+          tableName = 'users',
+          ACCEPTED_FIELDS = ['email', 'first_name', 'last_name', 'password', 'bio'];
+    Object.keys(data).forEach(key => {
+      if (!ACCEPTED_FIELDS.includes(key)) {
+            delete data[key];
+        }
+    });
+    super(tableName, pk, data);
   }
 
   refreshToken() {
-    return jwt.sign({pk: this.data[this.pk]}, SECRET, {expiresIn: JWT_EXP_THRESHOLD});
+    return jwt.sign({pk: this[this.pk]}, SECRET, {expiresIn: JWT_EXP_THRESHOLD});
   }
 }
 
