9f315ff404b4964bc670bef81bea8db582311721
trolleksii <trolleksii@gmail.com>
2018-12-02 21:34:46 -0500

    add user retrieval/update endpoints

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,17 +1,26 @@
 const express = require('express'),
-      router = express.Router();
+      router = express.Router(),
+      User = require('../models/User');
 
 // this one will be protected
-router.get('/', function login(req, res) {
-  res.json({message: 'get active user placeholder'})
+router.get('/', function (req, res) {
+  res.json(req.user);
 });
 
 // router.delete('/', function login(req, res) {
 //     res.json({message: 'delete user placeholder'})
 // });
 
-router.put('/', function login(req, res) {
-    res.json({message: 'update active user placeholder'})
+router.put('/', function (req, res) {
+  const user = new User(req.user);
+  user.data = req.body;
+  user.update()
+  .then(() => user.read())
+  .then(([data]) => {
+    delete data.password
+    return res.json(data);
+  })
+  .catch(res.json);
 });
 
 module.exports = router;
