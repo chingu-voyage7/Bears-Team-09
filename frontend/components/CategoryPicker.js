dd2d88cdffc3da86ff794c1166e3d558c93c4ad4
vaidotasp <vaidotasp@gmail.com>
2018-12-08 16:07:50 -0500

    work on city search and linting errors

--- frontend/components/CategoryPicker.js
+++ frontend/components/CategoryPicker.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 /*
 Component Fetches the category list from API and shows that as a dropdown
 Upon Category selection, component calls a callback function that triggers
@@ -7,61 +7,47 @@ parent compositional component to re-render Event list
 */
 
 class CategoryPicker extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       popupOpen: false,
-      activeCategory: 'category',
+      activeCategory: "category",
       categories: []
     };
     this.handleCategorySelection = this.handleCategorySelection.bind(this);
   }
 
-  handleCategorySelection(e){
-    //CB should be called here to indicate that we need to re-render event list based on new category
-    this.setState({popupOpen: false, activeCategory: e.target.innerHTML});
+  componentDidMount() {
+    // mock API call for categories
+    setTimeout(() => {
+      const fetchedCatsObj = [
+        { cat: "sport", id: 123 },
+        { cat: "games", id: 234 },
+        { cat: "outdoors", id: 999 },
+        { cat: "social", id: 852 },
+        { cat: "arts & culture", id: 444 }
+      ];
+      this.setState({ categories: fetchedCatsObj });
+    }, 1500);
   }
 
-  componentDidMount(){
-    //mock API call for categories
-    setTimeout(
-    function() {
-      const fetchedCatsObj = [
-        {cat: "sport",
-          id: 123
-        },
-        {cat: "games",
-          id: 234
-        },
-        {cat: "outdoors",
-          id: 999
-        },
-        {cat: "social",
-          id: 852
-        },
-        {cat: "arts & culture",
-          id: 444
-        }
-      ];
-      this.setState({categories: fetchedCatsObj});
-    }
-    .bind(this),
-    1500
-);
-
+  handleCategorySelection(e) {
+    // CB should be called here to indicate that we need to re-render event list based on new category
+    this.setState({ popupOpen: false, activeCategory: e.target.innerHTML });
   }
 
   render() {
     const { popupOpen, activeCategory, categories } = this.state;
-    const categoryList = categories.map( categoryObj => <CategoryListItem key={categoryObj.id} onClick={this.handleCategorySelection}>{categoryObj.cat}</CategoryListItem>);
+    const categoryList = categories.map(categoryObj => (
+      <CategoryListItem key={categoryObj.id} onClick={this.handleCategorySelection}>
+        {categoryObj.cat}
+      </CategoryListItem>
+    ));
 
     return (
-      <CategoryWrapper tabIndex="1" onBlur={() => this.setState({popupOpen: false})}>
-        <CategoryBox onClick={() => this.setState({popupOpen: true})}> {activeCategory}
-        </CategoryBox>
-        {popupOpen && <ul>
-          {categoryList}
-        </ul>}
+      <CategoryWrapper tabIndex="-1" onBlur={() => this.setState({ popupOpen: false })}>
+        <CategoryBox onClick={() => this.setState({ popupOpen: true })}> {activeCategory}</CategoryBox>
+        {popupOpen && <ul>{categoryList}</ul>}
       </CategoryWrapper>
     );
   }
@@ -70,12 +56,12 @@ class CategoryPicker extends Component {
 export default CategoryPicker;
 
 const CategoryWrapper = styled.div`
-
   padding: 2px;
   line-height: 25px;
 
-
-  &:focus { outline: none; }
+  &:focus {
+    outline: none;
+  }
   ul {
     list-style-type: none;
     margin: 2px 0px 0px 0px;
@@ -87,10 +73,10 @@ const CategoryWrapper = styled.div`
     background: white;
     padding: 5px;
     border-radius: 2px;
-    box-shadow: rgba(0,0,0,.5) 0 3px 10px 0;
+    box-shadow: rgba(0, 0, 0, 0.5) 0 3px 10px 0;
     z-index: 1;
   }
-`
+`;
 
 const CategoryBox = styled.div`
   width: 140px;
@@ -98,16 +84,16 @@ const CategoryBox = styled.div`
   cursor: pointer;
   text-align: center;
   font-size: 1rem;
-  border: 1px solid rgba(0,0,0,.12);
+  border: 1px solid rgba(0, 0, 0, 0.12);
   color: #757575;
-  background-color: #FFF;
+  background-color: #fff;
   border-radius: 2px;
   transition: all 100ms ease-out;
   &:hover {
     background: purple;
     color: white;
   }
-`
+`;
 
 const CategoryListItem = styled.li`
   text-transform: capitalize;
@@ -119,4 +105,4 @@ const CategoryListItem = styled.li`
     color: white;
     background: black;
   }
-`
+`;
--- frontend/components/LocationSearch.js
+++ frontend/components/LocationSearch.js
@@ -1,80 +1,120 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
 class LocationSearch extends Component {
   constructor() {
     super();
     this.state = {
-      currentInput: '',
+      currentInput: "",
       suggestionPopup: false,
       suggestions: [],
       locations: []
     };
     this.handleChange = this.handleChange.bind(this);
     this.clearInput = this.clearInput.bind(this);
-
-  }
-
-  handleChange(e) {
-    const { locations } = this.state;
-    this.setState({ currentInput: e.target.value });
-    const currentValue = e.target.value;
-    const suggestions = locations.filter(city => city.toLowerCase().match(currentValue.toLowerCase(), 'gmi'));
-    this.setState({ suggestions });
-  }
-
-  clearInput() {
-    this.setState({ currentInput: '' });
+    this.select = this.select.bind(this);
   }
 
   componentDidMount() {
-    //mock the API call
-    setTimeout(
-      function() {
-        const locations = [
-          { country: "Canada", city: "Toronto" },
-          { country: "Germany", city: "Berlin" },
-          { country: "United States", city: "New York" },
-          { country: "United States", city: "Seattle" },
-          { country: "United Kingdom", city: "London" },
-          { country: "France", city: "Paris" },
-          { country: "Canada", city: "Victoria" },
-        ];
-        const locationArray = locations.map(locationObject => locationObject.city);
-        this.setState({ locations: locationArray });
-      }
-      .bind(this),
-      1500
-    );
+    // mock the API call
+
+    setTimeout(() => {
+      const locations = [
+        {
+          country: "Canada",
+          city: "Toronto"
+        },
+        {
+          country: "Germany",
+          city: "Berlin"
+        },
+        {
+          country: "United States",
+          city: "New York"
+        },
+        {
+          country: "United States",
+          city: "Seattle"
+        },
+        {
+          country: "United Kingdom",
+          city: "London"
+        },
+        {
+          country: "France",
+          city: "Paris"
+        },
+        {
+          country: "Canada",
+          city: "Victoria"
+        }
+      ];
+      const locationArray = locations.map(locationObject => locationObject.city);
+      this.setState({
+        locations: locationArray
+      });
+    }, 1500);
+  }
+
+  handleChange(e) {
+    const { locations, suggestions } = this.state;
+    this.setState({ currentInput: e.target.value });
+    const currentValue = e.target.value;
+
+    // Filter locations based on user current input
+    const filterSuggestions =
+      currentValue.length !== 0
+        ? locations.filter(city => city.toLowerCase().match(currentValue.toLowerCase(), "gmi"))
+        : [];
+    this.setState({ suggestions: filterSuggestions });
+
+    // Check if suggestion popup needs to hide or show
+    if (suggestions.length === 0 || currentValue === "") {
+      this.setState({ suggestionPopup: false });
+    } else if (suggestions.length !== 0) {
+      this.setState({ suggestionPopup: true });
+    }
+  }
+
+  clearInput() {
+    this.setState({ currentInput: "" });
+    this.setState({ suggestions: [] });
+    this.setState({ suggestionPopup: false });
+  }
+
+  select(e, location) {
+    // handle suggestion selection
   }
 
   render() {
     const { currentInput, suggestions, suggestionPopup } = this.state;
-    const suggestionList = suggestions.map(city => <li>{city}</li>);
+    const suggestionList = suggestions.map(city => (
+      <SuggestionItem onClick={e => this.select(e, city)}>{city}</SuggestionItem>
+    ));
     return (
-      <div style={{width: 150}}>
-      <SearchBarWrapper>
-        <StyledSearchBar placeholder="location" type="text" onChange={this.handleChange} value={this.state.currentInput}/>
-        {currentInput.length !== 0 && <ClearButton onClick={this.clearInput}>x</ClearButton>}
-      </SearchBarWrapper>
-      {suggestionPopup && <ul>{suggestionList}</ul>}
-      </div>
+      <>
+        <SearchBarWrapper>
+          <StyledSearchBar placeholder="location" type="text" onChange={this.handleChange} value={currentInput} />
+          {currentInput.length !== 0 && <ClearButton onClick={this.clearInput}>x</ClearButton>}
+          {suggestionPopup && <SuggestionList>{suggestionList}</SuggestionList>}
+        </SearchBarWrapper>
+      </>
     );
   }
 }
 
 export default LocationSearch;
 
-const SearchBarWrapper = styled.div `
+const SearchBarWrapper = styled.div`
   position: relative;
   vertical-align: middle;
   line-height: 1.3;
-`
+`;
 
-const StyledSearchBar = styled.input `
+const StyledSearchBar = styled.input`
   text-align: center;
   font-size: 1rem;
-  border: 1px solid rgba(0,0,0,.12);
+  border: 1px solid rgba(0, 0, 0, 0.12);
   color: #757575;
   display: block;
   width: 150px;
@@ -82,21 +122,60 @@ const StyledSearchBar = styled.input `
   border-radius: 3px;
   transition: all 0.1s 0.1s ease-in;
   outline: none;
-  background-color: #FFF;
+  background-color: #fff;
   transition: all 100ms ease-out;
 
+  &:focus::-webkit-input-placeholder {
+    color: transparent;
+  }
+  &:focus:-moz-placeholder {
+    color: transparent;
+  }
+  &:focus::-moz-placeholder {
+    color: transparent;
+  }
+  &:focus:-ms-input-placeholder {
+    color: transparent;
+  }
 `;
 
-const ClearButton = styled.button `
-    font-size: 1.1rem;
-    position: absolute;
-    right: -2px;
-    top: 50%;
-    transform: translateY(-50%);
-    background: transparent;
-    border: none;
-    outline: none;
-    font-weight: 600;
-    color: #999;
-    cursor: pointer;
+const SuggestionList = styled.ul`
+  list-style-type: none;
+  width: 140px;
+  margin: 2px 0px 0px 0px;
+  padding: 0;
+  position: absolute;
+  display: grid;
+  grid-gap: 5px;
+  background: white;
+  padding: 5px;
+  border-radius: 2px;
+  box-shadow: rgba(0, 0, 0, 0.5) 0 3px 10px 0;
+  z-index: 1;
+`;
+
+const SuggestionItem = styled.li`
+  text-transform: capitalize;
+  text-align: left;
+  cursor: pointer;
+  padding: 2px;
+  border-radius: 2px;
+  &:hover {
+    color: white;
+    background: black;
+  }
+`;
+
+const ClearButton = styled.button`
+  font-size: 1.1rem;
+  position: absolute;
+  right: -2px;
+  top: 50%;
+  transform: translateY(-50%);
+  background: transparent;
+  border: none;
+  outline: none;
+  font-weight: 600;
+  color: #999;
+  cursor: pointer;
 `;
