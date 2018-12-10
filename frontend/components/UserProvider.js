819fe7f75701a69c8e2c5de67e726a95c5c8c9a0
lkaratun <lev.karatun@gmail.com>
2018-12-10 01:27:10 -0500

    Store user context in localStorage

--- frontend/components/UserProvider.js
+++ frontend/components/UserProvider.js
@@ -4,19 +4,33 @@ import PropTypes from "prop-types";
 const UserContext = React.createContext();
 class UserProvider extends Component {
   state = {
-    user: "guest",
     loggedIn: false,
-    firstName: undefined,
-    lastName: undefined
+    firstName: null,
+    lastName: null,
+    email: null
   };
 
+  componentDidMount() {
+    this.setState({
+      loggedIn: localStorage.getItem("loggedIn") === "true",
+      firstName: localStorage.getItem("firstName"),
+      lastName: localStorage.getItem("lastName"),
+      email: localStorage.getItem("email")
+    });
+  }
+
   logIn = data => {
+    console.log({ data });
     console.log(`Logged in as ${data.profileObj.givenName} ${data.profileObj.familyName}`);
     this.setState({ loggedIn: true, firstName: data.profileObj.givenName, lastName: data.profileObj.familyName });
+    Object.entries(data.profileObj).forEach(([key, value]) => {
+      localStorage.setItem(key, value);
+    });
+    localStorage.setItem("loggedIn", "true");
   };
 
   logOut = () => {
-    this.setState({ loggedIn: false });
+    this.setState({ loggedIn: false, firstName: null, lastName: null, email: null });
   };
 
   updateUser = newName => {
