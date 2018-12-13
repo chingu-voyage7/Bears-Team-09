46813c040c5b9f52f99c0c6a49e96a007c4e4986
vaidotasp <vaidotasp@gmail.com>
2018-12-13 06:12:20 -0500

    improving the filtering functionality

--- frontend/components/EventList.js
+++ frontend/components/EventList.js
@@ -1,22 +1,40 @@
-import React, { Component } from 'react';
-import EventListItem from './EventListItem';
+import React, { Component } from "react";
+import { format } from "date-fns";
+import EventListItem from "./EventListItem";
+
+// we would use this helper function to filter events, may need to push out //to utils folder for cleaner looking component
+function applyEventsFilter(events, filters = null) {
+  if (!filters) return events;
+  const filterArr = Object.keys(filters);
+  const filteredEvents = events.filter(event => {
+    for (let i = 0; i < filterArr.length; i++) {
+      const currFilter = filterArr[i];
+      // handle unset state which is null and should be ignored
+      if (filters[currFilter] === null) break;
+      // special handling of the date filter as we need to format it
+      if (currFilter === "datefrom") {
+        if (format(new Date(filters[currFilter]), "yyy-MM-dd") !== format(new Date(event[currFilter]), "yyy-MM-dd")) {
+          return false;
+        }
+        return true;
+      }
+      // handle everything else
+      if (filters[currFilter] !== event[currFilter]) {
+        return false;
+      }
+    }
+    return true;
+  });
+  return filteredEvents;
+}
 
 class EventList extends Component {
   render() {
-    const events = [
-      { id: 1, title: 'biking', description: 'special trail' },
-      { id: 2, title: 'baking', description: 'special cake' },
-      {
-        id: 3,
-        title: 'hiking',
-        description: 'lots of bears on this hike need a friend'
-      },
-      { id: 4, title: 'concert', description: 'I do not wanna go alone' },
-      { id: 5, title: 'workout', description: 'Lifting heavy need a spotter' }
-    ];
+    const { events, filters } = this.props;
+    const eventsToShow = applyEventsFilter(events, filters);
     return (
       <div>
-        {events.map(event => (
+        {eventsToShow.map(event => (
           <EventListItem key={event.id} {...event} />
         ))}
       </div>
