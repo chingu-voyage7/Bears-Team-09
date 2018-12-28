9a2f81a1c1a0882a2f04d38be95a83db30d9db04
lkaratun <lev.karatun@gmail.com>
2018-12-28 15:43:18 -0500

    Change footer styling

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Footer = () => (
   <StyledFooter>
     <p>
       Project developed as part of Chingu Voyage 7 cohort, by&nbsp;
-      <a href="https://github.com/nealthom" rel="noreferrer noopener" target="_blank">@nealthom&nbsp;</a>
-      <a href="https://github.com/lkaratun" rel="noreferrer noopener" target="_blank">@lkaratun&nbsp;</a>
-      <a href="https://github.com/trolleksii" rel="noreferrer noopener" target="_blank">@trolleksii&nbsp;</a>
+      <a href="https://github.com/nealthom" rel="noreferrer noopener" target="_blank">
+        @nealthom&nbsp;
+      </a>
+      <a href="https://github.com/lkaratun" rel="noreferrer noopener" target="_blank">
+        @lkaratun&nbsp;
+      </a>
+      <a href="https://github.com/trolleksii" rel="noreferrer noopener" target="_blank">
+        @trolleksii&nbsp;
+      </a>
       and&nbsp;
-      <a href="https://github.com/vaidotasp" rel="noreferrer noopener" target="_blank">@vaidotasp&nbsp;</a>
+      <a href="https://github.com/vaidotasp" rel="noreferrer noopener" target="_blank">
+        @vaidotasp&nbsp;
+      </a>
     </p>
-    <br />
     <div>
-      <p>Source Code</p>
-      <a
-        rel="noreferrer noopener"
-        target="_blank"
-        href="https://github.com/chingu-voyage7/Bears-Team-09"
-      >
-        <img src="./static/gh-logo-32.png" alt="github-logo" />
+      <a rel="noreferrer noopener" target="_blank" href="https://github.com/chingu-voyage7/Bears-Team-09">
+        <p> Source Code</p>
       </a>
     </div>
   </StyledFooter>
@@ -30,22 +32,23 @@ export default Footer;
 const StyledFooter = styled.footer`
   color: white;
   background: black;
-  padding: 30px;
+  padding: 5px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   text-align: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 
   p {
-    padding: 0;
-    margin: 0;
+    margin: 5px;
     font-size: 0.7rem;
   }
 
   a {
     color: inherit;
     text-decoration: none;
-
   }
 
   img {
