c7070bd6c20b6cd3627bb0e9b6bb498cfd9b6f07
lkaratun <lev.karatun@gmail.com>
2019-01-03 21:31:10 -0500

    WIP

---
+++ frontend/components/ImageUploader.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class ImageUploader extends Component {
+  state = { selectedFile: null };
+
+  handleFileSelect = e => {
+    this.setState({ selectedFile: e.target.files[0] });
+    const formData = new FormData();
+    formData.append("myFile", this.state.selectedFile, this.state.selectedFile.name);
+    // axios.post("my-domain.com/file-upload", formData);
+  };
+
+  handleClick = () => this.selectFile.click();
+
+  render() {
+    return (
+      <div>
+        <input
+          type="file"
+          onChange={this.handleFileSelect}
+          ref={element => {
+            this.selectFile = element;
+          }}
+          accept="image/png, image/jpeg"
+          style={{ display: "none" }}
+        />
+        <button type="button" onClick={this.handleClick}>
+          Upload new profile picture
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ImageUploader;
