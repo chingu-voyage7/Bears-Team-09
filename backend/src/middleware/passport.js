d877ad6783717db332c8c86f7a7bf219a910659e
trolleksii <trolleksii@gmail.com>
2019-01-13 10:32:06 -0500

    custom google authentication strategy

--- backend/src/middleware/passport.js
+++ backend/src/middleware/passport.js
@@ -2,15 +2,13 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 const passportJWT = require('passport-jwt');
 const LocalStrategy = require('passport-local').Strategy;
-const BearerStrategy = require('passport-http-bearer').Strategy;
 const axios = require('axios');
+const GoogleStrategy = require('./googleStrategy');
 const User = require('../models/User');
 
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 const secret = process.env.JWT_SECRET || 'Default_JWT-Secret';
-
-BearerStrategy.prototype._challenge = (err, desc) => `${err}: ${desc}`;
 
 passport.use(
   new LocalStrategy(
@@ -22,8 +20,8 @@ passport.use(
       const user = new User({email});
       return user.read()
         .then(([data]) => bcrypt.compare(password, data.password))
-        .then(isAuthenticated => isAuthenticated ? done(null, user) : done(null, false, 'Incorrect username or password'))
-        .catch(err => done(null, false, err.message));
+        .then(isAuthenticated => isAuthenticated ? done(null, user) : done(null, null, 'Incorrect username or password'))
+        .catch(err => done(null, null, err));
     }
 ));
 
@@ -36,18 +34,17 @@ passport.use(
       const user = new User({id: jwtPayload.id});
       return user.read()
         .then(() => done(null, user))
-        .catch(err => done(null, false, err.message));
+        .catch(err => done(null, null, err));
       }
 ));
 
 passport.use(
-    new BearerStrategy({passReqToCallback: true},
-        (req, token, done) => {
-            console.log(req);
+    new GoogleStrategy(
+        (token, done) => {
             let user;
             let newUser;
             // Get info from google
-            axios.get(
+            return axios.get(
                 'https://www.googleapis.com/oauth2/v3/userinfo',
                 {
                     headers: {"Authorization": `Bearer ${token}`}
@@ -62,8 +59,7 @@ passport.use(
                     email: data.email,
                     first_name: data.given_name,
                     last_name: data.family_name,
-                    image: data.picture,
-                    password: 'qweqweqwe'});
+                    image: data.picture});
                 return user.read();
             })
             // if not - create one
@@ -75,11 +71,8 @@ passport.use(
                 throw err;
             })
             // and then return
-            .then(() => {
-                req.user = user;
-                done(null, user);
-            })
-            .catch((err) => done(null, false, err.message));
+            .then(() => done(null, user))
+            .catch((err) => done(null, null, err));
         }
     )
 );
@@ -87,7 +80,7 @@ passport.use(
 module.exports = (strategy) => (req, res, next) =>
     passport.authenticate(strategy, {session: false}, (err, user, info) => {
         if (err || !user) {
-            return res.status(401).json({message: info});
+            return res.status(401).json({message: info.message});
         }
         req.login(user, {session: false}, (error) => {
             if (error) {
