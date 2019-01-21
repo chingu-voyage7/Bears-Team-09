01b23a08c49fa912e0dba93e6fddb4c784a6483d
vaidotasp <vaidotasp@gmail.com>
2019-01-21 11:30:27 -0500

    date formatting

--- frontend/pages/event.js
+++ frontend/pages/event.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import styled from "styled-components";
+import { format } from "date-fns";
 import { withRouter } from "next/router";
 import PropTypes from "prop-types";
 import MainLayout from "../components/MainLayout";
@@ -8,17 +9,22 @@ import MainLayout from "../components/MainLayout";
 export class event extends Component {
   state = {
     name: "",
-    startDate: "",
+    dateFrom: "",
+    dateTo: "",
     title: "",
     image: "",
-    description: ""
+    description: "",
+    city: "",
+    country: "",
+    maxPeople: "",
+    minPeople: ""
   };
 
   async componentDidMount() {
+    const { router } = this.props;
     const { tokenCtx } = this.context;
     const token = tokenCtx || localStorage.getItem("token");
     const AuthStr = `Bearer ${token}`;
-    const { router } = this.props;
     const currentEvent = await axios({
       method: "get",
       url: `http://localhost:8000/events/${router.query.id}`,
@@ -26,22 +32,56 @@ export class event extends Component {
         Authorization: AuthStr
       }
     });
-    const { name, date_from: startDate, description, image } = currentEvent.data;
-    this.setState({ name, startDate, description, image });
+    console.log(currentEvent.data);
+    const {
+      name,
+      date_from: dateFrom,
+      date_to: dateTo,
+      description,
+      image,
+      city,
+      country,
+      max_people: maxPeople,
+      min_people: minPeople
+    } = currentEvent.data;
+    const dateFromFormat = format(dateFrom, "MMMM DD YYYY");
+    const dateToFormat = format(dateTo, "MMMM DD YYYY");
+
+    this.setState({
+      name,
+      dateFrom: dateFromFormat,
+      dateTo: dateToFormat,
+      description,
+      image,
+      city,
+      country,
+      maxPeople,
+      minPeople
+    });
   }
 
   render() {
-    const { name, startDate, description, image } = this.state;
-    // const theme = { background: `center / cover no-repeat url(${image})` };
-    // let startDate = "2019-07-19";
-    // let description = "some description goes here";
-    // let name = "this is a name";
+    const { name, dateFrom, dateTo, description, image, city, country } = this.state;
+
+    // FIXME: This needs to be tested when backend has images to show, we need to make sure path works with DB
+    const eventImage = image || "../static/stock-event.jpg";
     return (
       <MainLayout>
         <Container>
-          <DateTime>{startDate}</DateTime>
-          <Title>{description}</Title>
-          <Host>Hosted by: {name}</Host>
+          <EventCard>
+            <Name>{name}</Name>
+            <InfoWrapper>
+              <InfoPanel>
+                <Description>Description: {description}</Description>
+                <div>
+                  Location: {city}, {country}
+                </div>
+                <DateTime>Date/Time: {dateFrom}</DateTime>
+                <DateTime>Date/Time: {dateTo}</DateTime>
+              </InfoPanel>
+              <EventImage src={eventImage} alt="people in a group" />
+            </InfoWrapper>
+          </EventCard>
         </Container>
       </MainLayout>
     );
@@ -49,24 +89,53 @@ export class event extends Component {
 }
 
 const Container = styled.div`
+  margin-top: 50px;
+  margin-bottom: 50px;
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
 `;
 
+const EventCard = styled.div`
+  width: 50vw;
+  min-height: 60vh;
+  padding: 2em;
+  margin: 0.5em 0;
+  border-radius: 0.25em;
+  background: hsla(232, 59%, 50%, 0.05);
+  box-shadow: 2px 2px 11px -4px rgba(0, 0, 0, 0.3);
+`;
+
+const InfoWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-gap: 30px;
+`;
+
+const InfoPanel = styled.div`
+  border: 1px solid grey;
+`;
+
+const EventImage = styled.img`
+  width: 300px;
+  border-radius: 10px;
+`;
+
 const DateTime = styled.div`
   color: grey;
 `;
-const Host = styled.div`
-  color: grey;
+const Name = styled.div`
+  text-transform: capitalize;
+  font-size: 3rem;
+  margin-bottom: 25px;
 `;
-const Title = styled.div`
+const Description = styled.div`
   font-size: 2em;
 `;
 
 event.propTypes = {
-  router: PropTypes.element.isRequired
+  router: PropTypes.object.isRequired
 };
 
 export default withRouter(event);
