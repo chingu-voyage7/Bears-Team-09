eb3259d71f6c2d3ebdbf6c86fa137e361398a4a6
trolleksii <trolleksii@gmail.com>
2019-01-26 13:18:58 -0500

    removed certificates autoissue

--- deploy/step2/3_host.tf
+++ deploy/step2/3_host.tf
@@ -23,10 +23,6 @@ resource "tls_private_key" "ssh_key" {
   rsa_bits  = 4096
 }
 
-output "private_key" {
-  value = "${tls_private_key.ssh_key.private_key_pem}"
-}
-
 resource "aws_key_pair" "my_keypair" {
   public_key = "${tls_private_key.ssh_key.public_key_openssh}"
 }
@@ -82,7 +78,8 @@ resource "aws_instance" "server" {
         inline = [
             "sudo yum update -y",
             "sudo amazon-linux-extras install docker -y",
-            "sudo service docker start"
+            "sudo service docker start",
+            "sudo mkdir -p /etc/nginx/certs"
         ]
     }
     tags {
@@ -94,37 +91,38 @@ resource "aws_instance" "server" {
 data "template_file" "nginx_gateway" {
     template = "${file("deploy/step2/dropins/nginx.tpl")}"
     vars = {
-        frontend_name = "${var.frontend_image}"
-        backend_name = "${var.backend_image}"
+        frontend_name = "frontend"
+        backend_name = "backend"
         server_name = "${var.domain_name}"
     }
 }
 
 # These provisioners will run only once on instance creation
-resource "null_resource" "copy_certificates" {
-    connection {
-        host = "${aws_instance.server.public_ip}"
-        user  = "ec2-user"
-        private_key = "${tls_private_key.ssh_key.private_key_pem}"
-    }
-    provisioner "file" {
-        content = "${acme_certificate.certificate.certificate_pem}"
-        destination = "/tmp/certificate.pem"
-    }
-    provisioner "file" {
-        content = "${acme_certificate.certificate.private_key_pem}"
-        destination = "/tmp/private_key.pem"
-    }
-    provisioner "remote-exec" {
-        inline = [
-            "sudo mkdir -p /etc/nginx/certs",
-            "sudo mv /tmp/certificate.pem /etc/nginx/certs/certificate.pem",
-            "sudo mv /tmp/private_key.pem /etc/nginx/certs/private_key.pem",
-            "sudo chown -R root:root /etc/nginx",
-            "sudo chmod 600 /etc/nginx/certs/*"
-        ]
-    }
-}
+# resource "null_resource" "copy_certificates" {
+#     triggers {
+#         cert = "${acme_certificate.certificate.certificate_pem}"
+#     }
+#     connection {
+#         host = "${aws_instance.server.public_ip}"
+#         user  = "ec2-user"
+#         private_key = "${tls_private_key.ssh_key.private_key_pem}"
+#     }
+#     provisioner "file" {
+#         content = "${acme_certificate.certificate.certificate_pem}"
+#         destination = "/tmp/certificate.pem"
+#     }
+#     provisioner "file" {
+#         content = "${acme_certificate.certificate.private_key_pem}"
+#         destination = "/tmp/private_key.pem"
+#     }
+#     provisioner "remote-exec" {
+#         inline = [
+#             "sudo mv -f /tmp/certificate.pem /tmp/private_key.pem /etc/nginx/certs",
+#             "sudo chown -R root:root /etc/nginx",
+#             "sudo chmod 600 /etc/nginx/certs/*"
+#         ]
+#     }
+# }
 
 # this provisioner will run each time when variable `image_tag` is changed
 resource "null_resource" "restart_apps" {
@@ -148,13 +146,13 @@ resource "null_resource" "restart_apps" {
             "sudo docker container prune -f",
             # Create docker network, so that our docker containers could reach each other
             "sudo docker network create --driver bridge ${var.project_name}-net",
-            "sudo docker run --name gateway -d --network ${var.project_name}-net -v /tmp/nginx.conf:/etc/nginx/nginx.conf -v /etc/nginx/certs/certificate.pem:/etc/nginx/certificate.pem -v /etc/nginx/certs/private_key.pem:/etc/nginx/private_key.pem -p 80:80 -p 443:443 nginx:stable-alpine",
+            "sudo docker run --name gateway -d --network ${var.project_name}-net --restart unless-stopped -v /tmp/nginx.conf:/etc/nginx/nginx.conf -v /etc/nginx/certs/certificate.pem:/etc/nginx/certificate.pem -v /etc/nginx/certs/private_key.pem:/etc/nginx/private_key.pem -p 80:80 -p 443:443 nginx:stable-alpine",
             # run migrations if any
             "sudo docker run --rm -e DATABASE_URL=postgres://${var.pg_user}:${var.pg_password}@${aws_db_instance.db.address}/${var.pg_db} ${var.backend_image}:${var.image_tag} npm run migrate up",
             # run backend
-            "sudo docker run --name backend -d --network ${var.project_name}-net --restart always -e PGHOST=${aws_db_instance.db.address} -e PGUSER=${var.pg_user} -e PGPASSWORD=${var.pg_password} -e PGDB=${var.pg_db} -e PGPORT=${var.pg_port} -e JWT_SECRET=${var.jwt_secret} -e JWT_EXP_THRESHOLD=${var.jwt_exp_threshold} -e CLOUDINARY_KEY=${var.cdn_key} -e CLOUDINARY_SECRET=${var.cdn_secret} -e NODE_ENV=${var.node_env} ${var.backend_image}:${var.image_tag}",
+            "sudo docker run --name backend -d --network ${var.project_name}-net --restart unless-stopped -e PGHOST=${aws_db_instance.db.address} -e PGUSER=${var.pg_user} -e PGPASSWORD=${var.pg_password} -e PGDB=${var.pg_db} -e PGPORT=${var.pg_port} -e JWT_SECRET=${var.jwt_secret} -e JWT_EXP_THRESHOLD=${var.jwt_exp_threshold} -e CLOUDINARY_KEY=${var.cdn_key} -e CLOUDINARY_SECRET=${var.cdn_secret} -e NODE_ENV=${var.node_env} ${var.backend_image}:${var.image_tag}",
             # run frontend
-            "sudo docker run --name frontend -d --network ${var.project_name}-net -e NODE_ENV=${var.node_env} -e BACKEND_URL=https://${var.domain_name}/api --restart always ${var.frontend_image}:${var.image_tag}"
+            "sudo docker run --name frontend -d --network ${var.project_name}-net -e NODE_ENV=${var.node_env} -e BACKEND_URL=https://${var.domain_name}/api --restart unless-stopped ${var.frontend_image}:${var.image_tag}"
         ]
     }
 }
--- deploy/step2/4_dns.tf
+++ deploy/step2/4_dns.tf
@@ -11,29 +11,29 @@ resource "aws_route53_record" "root_a_record" {
   records = ["${aws_instance.server.public_ip}"]
 }
 
-provider "acme" {
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
-}
+# provider "acme" {
+#   server_url = "https://acme-v02.api.letsencrypt.org/directory"
+# }
 
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-}
+# resource "tls_private_key" "private_key" {
+#   algorithm = "RSA"
+# }
 
-resource "acme_registration" "reg" {
-  account_key_pem = "${tls_private_key.private_key.private_key_pem}"
-  email_address   = "${var.letsenctypt_reg_email}"
-}
+# resource "acme_registration" "reg" {
+#   account_key_pem = "${tls_private_key.private_key.private_key_pem}"
+#   email_address   = "${var.letsenctypt_reg_email}"
+# }
 
-resource "acme_certificate" "certificate" {
-  account_key_pem           = "${acme_registration.reg.account_key_pem}"
-  common_name               = "${aws_route53_record.root_a_record.name}"
-  subject_alternative_names = ["www.${aws_route53_record.root_a_record.name}"]
-  dns_challenge {
-    provider = "route53"
-    config {
-      AWS_ACCESS_KEY_ID     = "${var.aws_access_key}"
-      AWS_SECRET_ACCESS_KEY = "${var.aws_secret_key}"
-      AWS_DEFAULT_REGION    = "${var.aws_region}"
-    }
-  }
-}
+# resource "acme_certificate" "certificate" {
+#   account_key_pem           = "${acme_registration.reg.account_key_pem}"
+#   common_name               = "${aws_route53_record.root_a_record.fqdn}"
+#   subject_alternative_names = ["www.${aws_route53_record.root_a_record.fqdn}"]
+#   dns_challenge {
+#     provider = "route53"
+#     config {
+#       AWS_ACCESS_KEY_ID     = "${var.aws_access_key}"
+#       AWS_SECRET_ACCESS_KEY = "${var.aws_secret_key}"
+#       AWS_DEFAULT_REGION    = "${var.aws_region}"
+#     }
+#   }
+# }
