551c3c657ae8163b2a91b1716f9c7686c1b04921
trolleksii <trolleksii@gmail.com>
2019-01-25 21:50:39 -0500

    adjustment

--- deploy/step2/4_dns.tf
+++ deploy/step2/4_dns.tf
@@ -22,8 +22,8 @@ resource "aws_route53_record" "root_a_record" {
 #}
 
 provider "acme" {
-  # server_url = "https://acme-v02.api.letsencrypt.org/directory"
-  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+  # server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
 }
 
 resource "tls_private_key" "private_key" {
@@ -38,7 +38,7 @@ resource "acme_registration" "reg" {
 resource "acme_certificate" "certificate" {
   account_key_pem           = "${acme_registration.reg.account_key_pem}"
   common_name               = "${aws_route53_record.root_a_record.name}"
-  subject_alternative_names = ["${aws_instance.server.public_dns}"]
+  subject_alternative_names = ["www.${aws_route53_record.root_a_record.name}"]
   dns_challenge {
     provider = "route53"
     config {
