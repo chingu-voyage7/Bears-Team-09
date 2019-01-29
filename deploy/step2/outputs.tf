9f1b6d88ddf5124da6bc20835fe0aa9ab4d7e0c3
trolleksii <trolleksii@gmail.com>
2019-01-28 20:23:57 -0500

    remove output for now

--- deploy/step2/outputs.tf
+++ deploy/step2/outputs.tf
@@ -1,6 +1,6 @@
-output "amazon_issued_dns" {
-  value = "${aws_instance.server.public_dns}"
-}
+# output "amazon_issued_dns" {
+#   value = "${aws_instance.server.public_dns}"
+# }
 
 # output "registered_fqdn" {
 #   value = "${aws_route53_record.root_a_record.name}"
