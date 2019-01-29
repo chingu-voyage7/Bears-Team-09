a74fefd6bd0a959fcfe1ffd7524e1851d885b459
trolleksii <trolleksii@gmail.com>
2019-01-28 19:10:05 -0500

    refactor to read db data from file

--- backend/src/utils/populate_db.js
+++ backend/src/utils/populate_db.js
@@ -1,40 +1,19 @@
 // DB Population script, use settings from ../models/db.js
-
+const fs = require('fs');
 const User = require('../models/User');
 const Place = require('../models/Place');
 const Activity = require('../models/Activity');
 const Event = require('../models/Event');
 const Attendee = require('../models/EventAttendee');
 
-const data = {
-    users: [
-        {email: "kenny2000@gmail.com", first_name: "Kenneth", last_name: "McCormick", password: "123456"},
-        {email: "stan.marsh@gmail.com", first_name: "Stanley", last_name: "Marsh", password: "123456"},
-        {email: "broflovsky.k@gmail.com", first_name: "Kyle", last_name: "Broflovsky", password: "123456"}
-    ],
-    places: [
-        {country: "Canada", city: "Toronto"},
-        {country: "United States of America", city: "Washington"}
-    ],
-    activities: [
-        {name: "Social events"},
-        {name: "Sport events"},
-        {name: "Music events"},
-    ],
-    events: [
-        {name: "Local ComicCon", author_id: 0, activity_id: 0, place_id: 0, date_from: "2019-03-09 09:00:00.0", date_to: "2018-03-10 20:00:00.0", min_people: 4, descriptio: "Join me for the first South Park Comic-Con"},
-        {name: "Basketball match", author_id: 2, activity_id: 1, place_id: 1, date_from: "2019-02-17 20:00:00.0", date_to: "2018-12-17 22:00:00.0", max_people: 2, descriptio: "Lorem ipsum Lorem ipsum Lorem ipsum"},
-        {name: "Music concert", author_id: 1, activity_id: 2, place_id: 1, date_from: "2019-02-24 19:00:00.0", date_to: "2018-02-24 21:00:00.0", max_people: 4, descriptio: "Lorem ipsum Lorem ipsum Lorem ipsum"},
-    ],
-    event_attendees: [
-        {event_id: 0, user_id: 0},
-        {event_id: 0, user_id: 1},
-        {event_id: 0, user_id: 2},
-        {event_id: 1, user_id: 2},
-        {event_id: 1, user_id: 1},
-        {event_id: 2, user_id: 1},
-    ]
-};
+const myArgs = process.argv.slice(2);
+if (myArgs.length > 1) {
+    throw new Error('Too many arguments, only one filename is accepted');
+} else if (myArgs.length == 0) {
+    throw new Error('Please pass filename to parse');
+}
+
+const data = JSON.parse(fs.readFileSync(myArgs[0]));
 
 Promise.all(
     // create users, activities and places first and append their id's to base object
@@ -62,4 +41,3 @@ Promise.all(
     });
 })
 .catch(console.log);
-
