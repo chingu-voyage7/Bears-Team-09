67e33f85b915484f1704dff18c9f2c9a09d59f3f
Lev Karatun <lev.karatun@gmail.com>
2019-02-02 00:15:40 -0500

    Update db.js

--- backend/src/models/db.js
+++ backend/src/models/db.js
@@ -22,4 +22,4 @@ module.exports = {
 };
 
 // run migrations with
-// DATABASE_URL='postgres://postgres:"6?~6wA3WYfW(axLa*t*+7r?cbYS4"@event-buddies.cbzllsnbisgr.us-east-2.rds.amazonaws.com/postgres' npm run migrate up
+// DATABASE_URL=postgres://postgres:123456@docker.vm/postgres npm run migrate up
