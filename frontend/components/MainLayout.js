0fcb62811ce7491a4bedbd0715abc2aa7cd25f9a
lkaratun <lev.karatun@gmail.com>
2019-02-04 00:05:25 -0500

    Update main page title

--- frontend/components/MainLayout.js
+++ frontend/components/MainLayout.js
@@ -12,6 +12,7 @@ const MainLayout = props => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <title>PairUp: find people to do things together</title>
       </Head>
       <GlobalStyle />
       <FlexContainer>
