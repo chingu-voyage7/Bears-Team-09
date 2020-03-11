8983a0a2c456678a96b5b572d3f691ceadfc733f
Lev Karatun <lev.karatun@gmail.com>
2020-03-10 22:36:16 -0400

    Delete deprecated Container wrapper

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,16 +1,14 @@
 import React from "react";
-import App, { Container } from "next/app";
+import App from "next/app";
 import UserProvider from "../components/UserProvider";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Container>
-        <UserProvider>
-          <Component {...pageProps} />
-        </UserProvider>
-      </Container>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
     );
   }
 }
