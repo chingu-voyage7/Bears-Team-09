7af0fc7cd9a9f2a6aac9844455a5817b5c2dc8d6
Lev Karatun <lev.karatun@gmail.com>
2020-03-12 20:31:30 -0400

    Save jwt in a cookie

--- frontend/components/TokenReader.js
+++ frontend/components/TokenReader.js
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Cookies from "js-cookie";
+import omit from "lodash/omit";
 
 export default ({ children }) => {
   const router = useRouter();
   useEffect(() => {
     if (router) {
       console.log(router);
-      if (router.query.token) {
-        localStorage.setItem("token", router.query.token);
-        router.replace({ pathname: router.pathname, query: {} });
+      if (router.query.jwt) {
+        // localStorage.setItem("jwt", router.query.token);
+        router.replace({ pathname: router.pathname, query: omit(router.query, "jwt") });
+        Cookies.set("jwt", router.query.jwt);
       }
     }
   });
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
+
 import MainLayout from "../components/MainLayout";
 
 export async function getServerSideProps(ctx) {
